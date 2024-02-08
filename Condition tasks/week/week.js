543a73d3dfc8f0d9bc738a25b90891f2e6ba4406
YujikJuneguy <elchin.hasanov087@gmail.com>
2024-02-08 18:11:35 +0400

    new tasks1

--- Condition tasks/week/week.js
+++ Condition tasks/week/week.js
@@ -1,1 +1,19 @@
- let day
+ let day +prompt('what day is it');
+ if(day === 1) {
+    document.write ('monday');
+ }else if (day === 2) {
+    document.write ('Tuesday');
+ }else if (day === 3) {
+    document.write ('Wednesday');
+ }else if (day === 4) {
+    document.write ('Thursday');
+ }else if (day === 5) {
+    document.write ('Friday');
+ }else if (day === 6) {
+    document.write ('Saturday');
+ }else if (day === 7) {
+    document.write ('Sunday');
+ }
+ else {
+    document.write(day id undefined)
+ }
