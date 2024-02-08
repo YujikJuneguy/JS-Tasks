a0ad8ac974ff93e52e80ef9acb147115b0b7ebea
YujikJuneguy <elchin.hasanov087@gmail.com>
2024-02-09 00:06:55 +0400

    done

--- Condition tasks/CURRENCY/currency.js
+++ Condition tasks/CURRENCY/currency.js
@@ -1,11 +1,20 @@
 let area = +prompt('ev nece kvadratdir?');
-let price = +prompt('ev neceyedir?');
-// let currency = prompt('AZN yoxsa dollar?');
-// let who = prompt('makler filan deyilsiz?');
-
-
-let result = area < 2000
-   alert('maraqli deyil')
-
-
-
+if (area < 2000) {
+    alert('2000 kvadratdan az olan ev mene maraqli deyil.');
+} 
+    let price = +prompt('ev neceyedir?');
+    if (price !== 99.999) {
+        alert('qiymet mehs 99.999 AZN olmalidir, maash aldim ');
+    }
+    let currency = prompt('AZN yoxsa dollar?');
+    if (currency != 'AZN') {
+        alert('mehs AZN olsun');
+    }
+    let who = prompt('makler filan deyilsiz?');
+    if (who !== 'ev yiyesi') {
+        alert('makler ve alvercilernen ish gormurem ');
+    } else{
+      alert('xoshuma geldi, aliram');
+    }
+            
+ 
--- age-calculator/yourage.js
+++ age-calculator/yourage.js
@@ -1,3 +1,4 @@
 const CURRENT_YEAR = 2024;
 let BY = prompt("type the year when  you born");
 console.log ("Your Current age is:", CURRENT_YEAR - BY);
+
