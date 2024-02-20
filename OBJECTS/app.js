55b66e45084edeecbae73f615c060c13838f2f20
YujikJuneguy <elchin.hasanov087@gmail.com>
2024-02-21 00:30:24 +0400

    objects task

---
+++ OBJECTS/app.js
@@ -0,0 +1,51 @@
+// Task-01
+
+// let My = {
+//     Name: 'Elchin',
+//     Gender: 'Male',
+//     DOB: '18 June 1987'
+// }
+// console.log (My.DOB);
+// _____________________________________________________________________________________________________
+
+// Task-02
+// const user = {
+//     Name: 'Elchin',
+//     Gender: 'Male',
+//     DOB: '18 June 1987'
+// }
+// user.age = '36'
+// delete user.DOB
+// console.log(user);
+// _____________________________________________________________________________________________________
+
+// Task-03
+// let user = {
+//     age: 30,
+//     retirementAge: 65,
+//     setAge: function(newAge) {
+//         this.age = newAge;
+//     },
+//     getYearsBeforeRetirement: function() {
+//         let yearsLeft = this.retirementAge - this.age;
+//         return yearsLeft > 0 ? yearsLeft : 0;
+//      }
+// };
+
+// console.log("İstifadəçinin yaşi: " + user.age);
+// let yearsLeft = user.getYearsBeforeRetirement();
+// console.log("Pensiyaya qədər qalan illərin sayi: " + yearsLeft);
+
+// Task-04
+// Kalkulyator obyekti yaradın.
+// Əsas riyazi hesablamaları yerinə yetirən və nəticəni yaddaşda saxlaya bilən kalkulyator obyekti yaradın.
+//   - Obyekt yaradın və toplama, çıxma, vurma, bölmə üçün metodlar əlavə edin.
+
+// calculator = {
+//     plus: '+',
+//     minus: '-',
+//     devide: '/',
+//     times: '*',
+//     equals: '='
+// }
+
