8396962b2650bb85f271e2b7c46ded438c0d8c15
YujikJuneguy <elchin.hasanov087@gmail.com>
2024-02-25 21:14:01 +0400

    new tasks

--- OBJECTS/app.js
+++ OBJECTS/app.js
@@ -187,8 +187,7 @@
 // _____________________________________________________________________________________
 
 // Taks -7
-// 7. existsAndTruthy() adinda funksiya yaradin. 2 ədəd argument alır: obyekt(obj) ve string (propertyName). Eger hemin obyektden o adda property varsa ve truthy-dirse, true qaytarsin eks halda false qaytarsin. Mes:
-// existsAndTruthy({a:1,b:2,c:3},'b') // true
+
 
 // existsAndTruthy({x:'a',y:null,z:'c'},'y') // false (obyektde 'y' var amma falsy-di ona gore false)
 // existsAndTruthy({x:'a',b:'b',z:undefined},'z') // false (obyektde 'z' yoxdu ona gore false)
